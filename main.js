0d8e03e42688c7419b264961af6370ff82d597aa
edenx909 <edenx909@gmail.com>
2024-06-18 12:01:46 +1000

    work more on IIFE

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
+//  assigning everything id need in JS
 let button = document.getElementsByTagName('button');
+let p = document.getElementById('winner');
+// create ids & functions without more work in HTML
 for (i = 0; i < button.length; i++) {
-    button[i].setAttribute('onClick', `clickFunc(${i+1})`)
+    button[i].setAttribute('onClick', `clickFunc(${i+1})`);
+    button[i].setAttribute('id', `${i+1}`);
 }
 
-
+// logic for the game
 let choices = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
 let winsConditions = [
     [1, 2, 3], 
     [4, 5, 6], 
@@ -18,52 +21,73 @@ let winsConditions = [
 ];
 
 
+// players info
+let player1name = document.getElementById('player1name');
+let player2name = document.getElementById('player2name');
+let div = document.getElementById('main');
+let restart = document.getElementById('restart');
+div.style.display = 'none';
+restart.style.display = 'none'
 let player1Choices = [];
 let player2Choices = [];
+let player1;
+let player2;
+let currentPlayer = 'player1';
 
-// function to compare to win condition
+// let players use their names, make the game visible once name is selected
+function assign() {
+    player1 = player1name.value;
+    player2 = player2name.value;
+    div.style.display = 'block'
+    restart.style.display = 'block'
+    p.textContent = `${player1}'s turn`
+}
 
+
+// check if anyone won
 function checkWin(playerChoices) {
    for (let condition of winsConditions) {
     if (condition.every(position => playerChoices.includes(position))) {
         return true;
-    } else {
-        return false;
-    }
+    } 
+    } return false;
    }
-}
 
+// reset everything for a new game
 function endGame() {
     player1Choices = [];
     player2Choices = [];
-    console.log("ready for new game");
+   div.style.display = 'none';
+    for (let i = 0; i < button.length; i++) {
+        button[i].textContent = '';
+    }
 }
 
-let currentPlayer = 'player1';
+
 // adding selected inputs to the array for inputs
 function clickFunc(id) {
+    // select all the buttons using their id
+    const button = document.getElementById(`${id}`)
     if (currentPlayer === 'player1' && !(player1Choices.includes(id))) {
         player1Choices.push(id);
-        console.log('Player 1 Chooses' + `${player1Choices}`);
-        console.log("Player 2 Turn");   
+        button.textContent = 'X';
         if (checkWin(player1Choices)) {
-            console.log('player 1 wins');
-            console.log('Game finished');
             endGame();
-        }
-
+            p.textContent = `${player1} Wins!`
+        } else {
         currentPlayer = 'player2';
+        p.textContent = `${player2}'s turn`
+        }
     } else if (currentPlayer === 'player2' && !(player2Choices.includes(id))) {
         player2Choices.push(id);
-        console.log('Player 2 Chooses' + `${player2Choices}`);
-        console.log("Player 1 Turn");
+        button.textContent = 'O';
         if (checkWin(player2Choices)) {
-            console.log('player 2 wins');
-            console.log('Game finished');
             endGame();
-        }
-
+            p.textContent = `${player2} Wins!`
+        } else {
         currentPlayer = 'player1';
+        p.textContent = `${player1}'s turn`
+        }
     }
 
 }
